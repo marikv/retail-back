84146d004e6fa8501ef7ced338521d622b2fbbea
Marin <marin.vartan@gmail.com>
2022-04-27 23:22:02 +0300

    update

--- resources/views/pdf/contractDealerConsimtamant.blade.php
+++ resources/views/pdf/contractDealerConsimtamant.blade.php
@@ -31,7 +31,7 @@ use Carbon\Carbon;
         <br>
         <span class="strong">IDNP / IDNO {{$data['dealer']['idno']}}</span><br>
         Consimt și confirm următoarele:<br>
-        - Prelucrarea datelor personale de către OCN „CREDIT BOX” SRL în scopul verificării veridicităţii informaţiei prezentate/ furnizate în scopul executării relaţiilor contractuale,
+        - Prelucrarea datelor personale de către <strong>OCN „CREDIT BOX” SRL</strong> în scopul verificării veridicităţii informaţiei prezentate/ furnizate în scopul executării relaţiilor contractuale,
         <br>
         - accesarea, procesarea, prelucrarea, verificarea, actualizarea, publicarea, transmiterea transfrontaliera, colectarea datelor mele personale privind IDNP, numele, prenumele, patronimicul, sexul, cetățenia, data și locul nașterii, imaginea grafică (fotografia), situația familială, situația militară, datele de geolocalizare/datele din trafic, datele personale ale membrilor familiei, datele din permisul de conducere, situația economică și financiară, datele privind bunurile imobile și mobile deținute, locul de domiciliu/viza de reședință, datele bancare, semnătura, datele din actele de stare civilă, datele de contact (numarul de telefon fix, mobil, adresă electronica), numărul dosarului de pensie, codul personal de asigurări sociale, codul asigurării medicale, datele genetice, datele biometrice și antropometrice, profesia și locul de muncă, informațiile despre formarea profesională (diplome, studii), caracteristici fizice, solicitate de OCN „CREDIT BOX” SRL in scopul executarii prevederilor contractuale;
         <br>
