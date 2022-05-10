6ac6021261f821c79a52e08bd4c6713374766dc3
Marin <marin.vartan@gmail.com>
2022-05-10 11:18:36 +0300

    b

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'retail.creditbox.md'),
+            'database' => env('DB_DATABASE', 'a_retail_prod'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
